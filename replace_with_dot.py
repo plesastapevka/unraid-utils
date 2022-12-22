82c6d3057f114a8520fae24f3a823f30aeb07997
Urban Vidovič <urbanfoundit@gmail.com>
2022-12-22 02:45:55 +0100

    feature(replacer): delete any hyphen a file name might contain

--- replace_with_dot.py
+++ replace_with_dot.py
@@ -9,7 +9,7 @@ def main():
     # Iterate over the files
     for file in files:
         # Replace spaces in the file name with dots
-        new_name = file.replace(" ", ".")
+        new_name = file.replace(" ", ".").replace("-", "")
         # Rename the file
         os.rename(file, new_name)
 
