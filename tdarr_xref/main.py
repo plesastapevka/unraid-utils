8bb5e91cef9cc4319ed51db2a1c07ba6d9491180
Urban Vidovič <urbanfoundit@gmail.com>
2023-02-25 19:18:08 +0100

    feat: implement cross referencing for shows

--- tdarr_xref/main.py
+++ tdarr_xref/main.py
@@ -35,7 +35,7 @@ def yes_or_no(question):
 
 def get_size(start_path='.'):
     if start_path.endswith(".mkv") or start_path.endswith(".mp4"):
-        return round(os.path.getsize(start_path)/(1024*1024*1024), 2)
+        return round(os.path.getsize(start_path) / (1024 * 1024 * 1024), 2)
     total_size = 0
     for dirpath, dirnames, filenames in os.walk(start_path):
         for f in filenames:
@@ -43,7 +43,7 @@ def get_size(start_path='.'):
             if not os.path.islink(fp):
                 total_size += os.path.getsize(fp)
 
-    return round(total_size/(1024*1024*1024), 2)
+    return round(total_size / (1024 * 1024 * 1024), 2)
 
 
 def delete_file(file, interactive=False):
@@ -66,6 +66,46 @@ def delete_file(file, interactive=False):
     return False
 
 
+def find_show_to_delete(element_info, torrent_info, root, element, is_parent=False):
+    to_delete = []
+    saved_size = 0
+    if torrent_info["title"] == "The Grand Tour":
+        print(torrent_info)
+        print(element_info)
+    if not is_parent and torrent_info["title"].lower() != element_info["title"].lower():
+        return 0, []
+    if element_info["season"] and torrent_info["season"] and torrent_info["season"] != element_info["season"]:
+        return 0, []
+    print(
+        f"Now searching for show {torrent_info['title']} S{torrent_info['season']}E{torrent_info['episode']} in {root}{element}")
+
+    if not os.path.isdir(root + element) and not (element.endswith(".mkv") or element.endswith(".mp4")):
+        return get_size(root + element), [root + element]
+    if os.path.isdir(root + element):
+        print(f"Now searching {root}{element}")
+        elements = os.listdir(root + element)
+        if len(elements) == 0:
+            return 0, root + element
+        for sub_element in elements:
+            size, to_delete_list = find_show_to_delete(PTN.parse(sub_element), torrent_info, root + element + "/",
+                                                       sub_element, True)
+            to_delete += to_delete_list
+            saved_size += size
+    print(element)
+    print(element_info)
+    print(torrent_info)
+    if element_info["episode"] and (element.endswith(".mkv") or element.endswith(".mp4")) and element_info[
+        "title"].lower() == torrent_info[
+        "title"].lower() and element_info["season"] == torrent_info["season"] and element_info["episode"] == \
+            torrent_info["episode"]:
+        to_delete += [root + element]
+        saved_size += get_size(root + element)
+        print(
+            f"Added {root + element} of size {get_size(root + element)} GB to delete list. Total size: {saved_size} GB")
+
+    return saved_size, to_delete
+
+
 def main(csv_file, root, force=False):
     if not root.endswith("/"):
         root = root + "/"
@@ -80,23 +120,36 @@ def main(csv_file, root, force=False):
             if row["newSize"] == '0':
                 continue
             torrent_path = row["_id"]
+            # TODO: enhance type fetching, idea: from run parameter
+            torrent_type = "movie" if torrent_path.split("/")[3] == "movies" else "show"
             torrent_name = os.path.basename(os.path.normpath(torrent_path))
             torrent_info = PTN.parse(torrent_name)
             try:
                 for element in os.listdir(root):
                     element_info = PTN.parse(element)
-                    if element_info["title"] == torrent_info["title"] and element_info["year"] == torrent_info["year"]:
-                        print(f"Staged for delete: {root + element} of size {get_size(root + element)} GB")
+                    if torrent_type == "show":
+                        size, to_delete_list = find_show_to_delete(element_info, torrent_info, root, element)
+                        to_delete += to_delete_list
+                        saved_size += size
+                        # print("\n\n")
+
+                    if torrent_type == "movie" and element_info["title"].lower() == torrent_info["title"].lower() and \
+                            (element_info["year"] and torrent_info["year"] and element_info["year"] == torrent_info[
+                                "year"]):
+                        print(
+                            f"Staged for delete: {torrent_type} {root + element} of size {get_size(root + element)} GB")
                         to_delete.append(os.path.join(root, element))
                         saved_size += get_size(root + element)
             except KeyError:
                 print(f"Error for torrent {torrent_info['title']}")
+
         if saved_size > 1000:
-            saved_size = f"{round(saved_size/1000, 2)} TB"
+            saved_size = f"{round(saved_size / 1000, 2)} TB"
         else:
             saved_size = f"{round(saved_size, 2)} GB"
         skipped = []
         deleted = 0
+        print(to_delete)
         if force or yes_or_no(f"Folders and files above will be deleted saving collectively {saved_size}. Continue?"):
             print("\nDeleting files...")
             for file in to_delete:
@@ -130,7 +183,8 @@ def main(csv_file, root, force=False):
 
             # Handle skipped files gracefully
             for file in skipped:
-                print(f"Skipped {file}, created/modified on {time.strftime('%d %B %Y %H:%M:%S', time.localtime(os.path.getctime(file)))}")
+                print(
+                    f"Skipped {file}, created/modified on {time.strftime('%d %B %Y %H:%M:%S', time.localtime(os.path.getctime(file)))}")
             deleted = 0
             if yes_or_no("Interactively delete skipped files?"):
                 for file in skipped:
